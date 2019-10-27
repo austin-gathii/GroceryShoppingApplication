124c1097900d33588fbe09198339c2b406999187
austin-gathii <austin.gathii@strathmore.edu>
2019-10-27 16:54:58 +0300

    third version

---
+++ GreenDelight/app/src/main/java/com/example/greendelight/AdminSystemStatisticsViewPage.java
@@ -0,0 +1,14 @@
+package com.example.greendelight;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminSystemStatisticsViewPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_system_statistics_view_page);
+    }
+}
---
+++ GreenDelight/app/src/main/java/com/example/greendelight/CustomerDeliveryInProgressViewPage.java
@@ -0,0 +1,14 @@
+package com.example.greendelight;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerDeliveryInProgressViewPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_delivery_in_progress_view_page);
+    }
+}
