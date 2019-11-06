e20dac3ee148e5ec14861bd44a6505899900dc1b
austin-gathii <austin.gathii@strathmore.edu>
2019-11-06 16:20:22 +0300

    almost done

---
+++ GreenDelight/app/src/main/java/com/example/greendelight/viewStandingOrdersPage.java
@@ -0,0 +1,14 @@
+package com.example.greendelight;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class viewStandingOrdersPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_standing_orders_page);
+    }
+}
