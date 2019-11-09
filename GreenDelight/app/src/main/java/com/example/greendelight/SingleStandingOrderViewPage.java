bf02a32ffc1c5c605473003b21adc10d023c7365
austin-gathii <austin.gathii@strathmore.edu>
2019-11-09 12:45:11 +0300

    final version

---
+++ GreenDelight/app/src/main/java/com/example/greendelight/SingleStandingOrderViewPage.java
@@ -0,0 +1,14 @@
+package com.example.greendelight;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SingleStandingOrderViewPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_standing_order_view_page);
+    }
+}
